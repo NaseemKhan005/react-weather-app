2153b0e2dde9481bd2958cec40ec430e9347b859
Naseem Khan <nk8257949@gamil.com>
2023-05-02 23:58:37 +0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import WeatherApp from "./weather-app/WeatherApp";
-import Todo from "./todo-app/Todo";
 
 const App = () => {
 	return (
 		<>
-			{/* <WeatherApp /> */}
-			<Todo />
+			<WeatherApp />
 		</>
 	);
 };
